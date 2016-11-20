f0461c613e0974f7ce709be73de30e73a9fe80b0
ilker delen <ilkerdelen@ilkers-MacBook-Pro-2.local>
2016-11-20 23:35:16 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+l = []
+
+# Print the type of `l` to confirm it's a list.
+print(type(l))
+l.append("January")
+l.append("February")
+print(l)
+ten = "10"
+eight = 8
+str_eight = str(eight)
+int_ten= int(ten)
+print (int_ten)
